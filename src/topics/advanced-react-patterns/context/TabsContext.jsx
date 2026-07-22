17e89de24fb6f6a116f806438e0003e4bde0122c
Monu <monu1041@gmail.com>
2026-07-22 22:19:16 +0530

    Added new topic

---
+++ src/topics/advanced-react-patterns/context/TabsContext.jsx
@@ -0,0 +1,24 @@
+import { createContext, useContext } from "react";
+
+/**
+ * Context used by all Tabs compound components.
+ */
+const TabsContext = createContext(null);
+
+/**
+ * Custom hook for consuming the Tabs context.
+ * Throws an error if used outside <Tabs>.
+ */
+export function useTabsContext() {
+    const context = useContext(TabsContext);
+
+    if (!context) {
+        throw new Error(
+            "Tabs components must be used inside <Tabs>."
+        );
+    }
+
+    return context;
+}
+
+export default TabsContext;
